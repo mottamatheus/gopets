2ec0a618b54b31f1e526494d71ce30d6e3afbbbb
Matheus Motta <matheusmottaq@gmail.com>
2020-11-26 21:33:24 -0300

    authorization and context api

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import GlobalStyles from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
 
   <>
     <GlobalStyles />
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Motta' }}>
+      <SignIn />
+    </AuthContext.Provider>
   </>
 );
 
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -8,9 +8,11 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import getValidationErrors from '../../utils/getValidationErrors';
+import AuthContext from '../../context/AuthContext';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const auth = useContext(AuthContext);
   const handleSubmit = useCallback(async (data: object) => {
     try {
       formRef.current?.setErrors({});
